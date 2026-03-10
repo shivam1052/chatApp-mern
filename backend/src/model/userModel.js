da6d0bd775c668032861e344531374e09a7edb83
Shivam Sharma <iamshivam152@gmail.com>
2026-03-11 04:56:19 +0530

    created user schema

---
+++ backend/src/model/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilepic: {
+      type: String,
+      default: "https://api.dicebear.com/9.x/avataaars/svg?seed=default",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
