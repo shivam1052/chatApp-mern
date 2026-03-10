577edcb7780570d050d7fc6d04940029d6951939
Shivam Sharma <iamshivam152@gmail.com>
2026-03-11 03:15:34 +0530

    setting up server via express

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
