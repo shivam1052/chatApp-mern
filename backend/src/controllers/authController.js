77fa42542299a65f673ca29931a87a22f6f40304
Shivam Sharma <iamshivam152@gmail.com>
2026-03-11 06:22:08 +0530

    created login controller and signup + login api tests success

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -2,6 +2,7 @@ import User from "../model/userModel.js";
 import bcrypt from "bcryptjs";
 import createToken from "../lib/token.js";
 
+//Signup controller
 export const signup = async (req, res) => {
   const { email, username, password } = req.body;
   try {
@@ -36,8 +37,33 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login page");
+//Login controller
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+    if (!matchPassword) {
+      res.status(400).json({ message: "Incorrect password!" });
+    }
+
+    createToken(user._id, res);
+    res.status(200).json({
+      message: "User logged in successfully!",
+      user: {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log("Login error!", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const logout = (req, res) => {
