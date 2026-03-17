2fb0d17fbd3baeb7f238f1e31c077135206921b1
Shivam Sharma <iamshivam152@gmail.com>
2026-03-17 18:33:21 +0530

    deployment fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://chatapp-mern-backend-xf7q.onrender.com/api",
   withCredentials: true,
 });
